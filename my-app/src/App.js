71ab8324fcdbe4b1ecae3bd65b0c06ac64fdb390
Kayla Lynch <kalynch1292@gmail.com>
2021-01-14 10:26:41 -0500

    errors with styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Table from "./components/Table";
+import Wrapper from "./components/Wrapper";
+import emplpyee from "./employee.json"
 
-const App = () => {
+function App() {
     return (
-        <div>
-            <Table />
-            <h1>Test</h1>
-        </div>
-    )
-}
+      <Wrapper>
+              name={employee[0].name}
+              email={employee[0].email}
+              dob={employee[0].dob}
+            </Wrapper>
+    );
+}           
 
-export default App;
+ export default App;
