62f649cca7a0230ae69ac52f33003be06a8cde14
Kayla Lynch <kalynch1292@gmail.com>
2021-01-14 09:27:07 -0500

    post tutoring

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Table from "./components/Table";
+
+const App = () => {
+    return (
+        <div>
+            <Table />
+            <h1>Test</h1>
+        </div>
+    )
+}
+
+export default App;
